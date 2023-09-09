5c70ced7b777b84e2c6fab4f17f3b069818f0224
Diego <diewentdiaz@gmail.com>
2023-09-09 23:10:14 +0200

    It is finally working, changed java version

--- src/main/java/com/miajon/jobsearch/controller/HelloWorldController.java
+++ src/main/java/com/miajon/jobsearch/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.miajon.jobsearch.controllers;
+package com.miajon.jobsearch.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
