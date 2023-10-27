09e6f5ada5478df54159ca2d3544a2fbb3c90dbc
Diego <diewentdiaz@gmail.com>
2023-10-27 19:28:00 +0200

    Fixed reload to pages

--- src/main/java/com/miajon/jobsearch/controller/ExpensesController.java
+++ src/main/java/com/miajon/jobsearch/controller/ExpensesController.java
@@ -7,33 +7,33 @@ import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ExpensesController {
 
-    private final ExpenseService expenseService;
+    private static final String MODEL_FORM = "expenseForm";
+    private static final String MODEL_EXPENSES_LIST = "expenses";
 
+    private static final String DEFAULT_VIEW = "expenses/expenses";
+    private final ExpenseService expenseService;
+    private Boolean isMonthlyList = false;
 
     public ExpensesController(ExpenseService expenseService) {
         this.expenseService = expenseService;
     }
 
-    private static final String MODEL_FORM = "expenseForm";
-    private static final String MODEL_EXPENSES_LIST = "expenses";
-    private static final String DEFAULT_VIEW = "expenses/expenses";
-
-    private Boolean isMonthlyList = false;
-
-
     private void refreshView(Model model) {
         Double totalAmount = expenseService.getTotalAmount();
         Double totalMonthlyAmount = expenseService.getTotalMonthlyAmount();
         model.addAttribute(MODEL_FORM, new ExpenseForm());
-        model.addAttribute("totalAmount",totalAmount == null ? 0 : totalAmount);
+        model.addAttribute("totalAmount", totalAmount == null ? 0 : totalAmount);
         //TODO: Maybe I can make it so its only one call to the database
         model.addAttribute("totalMonthlyAmount", totalMonthlyAmount == null ? 0 : totalMonthlyAmount);
-        model.addAttribute(MODEL_EXPENSES_LIST,(this.isMonthlyList) ?expenseService.findMonthlyExpenses() :expenseService.findAllExpenses());
+        model.addAttribute(MODEL_EXPENSES_LIST, (this.isMonthlyList) ? expenseService.findMonthlyExpenses() : expenseService.findAllExpenses());
         model.addAttribute("isMonthlyList", this.isMonthlyList);
 
     }
@@ -53,28 +53,33 @@ public class ExpensesController {
     }
 
     @PostMapping("/expenses")
-    public String addExpense(@ModelAttribute @Valid ExpenseForm expenseForm,BindingResult result, Model model) {
+    public String addExpense(@ModelAttribute @Valid ExpenseForm expenseForm, BindingResult result, Model model) {
 
         if (!result.hasErrors()) {
             Double realAmount = expenseForm.isIncome() ? expenseForm.getAmount() : -expenseForm.getAmount();
             Expense expense = new Expense(expenseForm.getConcept(), realAmount, expenseForm.getMonthly());
             expenseService.saveExpense(expense);
-            this.refreshView(model);
+            return redirectToOverview();
 
-        } else {
-            model.addAttribute(MODEL_EXPENSES_LIST, expenseService.findAllExpenses());
-            model.addAttribute(MODEL_FORM, expenseForm);
         }
+        model.addAttribute(MODEL_EXPENSES_LIST, expenseService.findAllExpenses());
+        model.addAttribute(MODEL_FORM, expenseForm);
         return DEFAULT_VIEW;
+
+
     }
 
     @PostMapping("/expenses/delete")
     public String deleteExpense(@RequestParam("itemId") String itemId, Model model) {
         expenseService.deleteExpense(itemId);
         this.refreshView(model);
-        return DEFAULT_VIEW;
+        return redirectToOverview();
     }
 
+
+    private String redirectToOverview() {
+        return "redirect:/" + (this.isMonthlyList ? "expenses/monthly" : "expenses");
+    }
 
 
 }
